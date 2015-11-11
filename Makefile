46aa9ce4edb19bd8339ae1a907c7bd247ca266c8
Patrick Mulder <mulder.patrick@gmail.com>
2015-11-11 21:14:30 +0100

    init

--- Makefile
+++ Makefile
@@ -1,3 +1,8 @@
+.PHONY: links
+
+links:
+	node render_links.js
+
 upload:
 	aws s3 sync --acl public-read static s3://embeddednodejs.com
 
---
+++ render_links.js
@@ -0,0 +1,32 @@
+var fs = require('fs');
+var findit = require('findit');
+var _ = require('underscore');
+
+var resourceTemplate = _.template(' \
+<h1><%= name %></h1> \n\
+  <% resources.forEach(function(resource) { %> \
+  <section class="addinfo">  \n\
+    <h3><%= resource.name %>  </h3>\n\
+    <ul> \n\
+    <% resource.links.forEach(function(link) { %> \
+      <% if (!link.link) { %>\
+        <li><%= link.name %></li> \n \
+        <% } else { %>  \
+        <li><a href="<%= link.link %>"><%= link.name %></a></li> \n \
+      <% } %> \
+    <% }); %>  \
+    </ul> \n\
+  </section>   \n\
+  <% }); %>  \
+');
+
+var renderLinks = function(file) {
+  var raw = fs.readFileSync(file);
+  var chapter = JSON.parse(raw.toString());
+  console.log(resourceTemplate({
+    name: chapter.name,
+    resources: chapter.resources
+  }));
+}
+
+renderLinks('./links/ch_3_nodejs_on_embedded_linux.json');

c5696373ed63291f465ba212826c0daf95cebc00
Patrick Mulder <mulder.patrick@gmail.com>
2016-07-22 09:26:06 +0200

    update links

--- render_links.js
+++ render_links.js
@@ -15,9 +15,11 @@ function slugify(text)
 
 var resourceTemplate = _.template(' \
 <h1 id="<%= slug %>"><%= name %></h1> \n\
+  <p><%= description %></p> \n\
   <% resources.forEach(function(resource) { %> \
   <section class="addinfo">  \n\
     <h3><%= resource.name %>  </h3>\n\
+    <p><%= resource.description %></p> \n\
     <ul> \n\
     <% resource.links.forEach(function(link) { %> \
       <% if (!link.link) { %>\
@@ -32,13 +34,20 @@ var resourceTemplate = _.template(' \
 ');
 
 var renderLinks = function(file) {
+  try {
   var raw = fs.readFileSync(file);
   var chapter = JSON.parse(raw.toString());
   console.log(resourceTemplate({
     slug: slugify(chapter.name),
     name: chapter.name,
+    description: chapter.description,
     resources: chapter.resources
   }));
+  } catch (ex) {
+    console.log(file);
+    console.log(ex);
+    process.exit(0);
+  }
 }
 
 console.log('<article class="overview chapter">');
@@ -50,36 +59,48 @@ renderLinks('./links/ch_2_arduino_and_javascript.json');
 console.log('</article>');
 
 console.log('<article class="overview chapter">');
-renderLinks('./links/ch_3_nodejs_on_embedded_linux.json');
+renderLinks('./links/ch_3_espruino.json');
 console.log('</article>');
 
 console.log('<article class="overview chapter">');
-renderLinks('./links/ch_4_components_for_prototyping.json');
+renderLinks('./links/ch_4_tessel2.json');
 console.log('</article>');
 
 console.log('<article class="overview chapter">');
-renderLinks('./links/ch_5_nodejs_libraries_for_hardware.json');
+renderLinks('./links/ch_5_internet_enabled_microcontrollers.json');
 console.log('</article>');
 
 console.log('<article class="overview chapter">');
-renderLinks('./links/ch_6_building_servers.json');
+renderLinks('./links/ch_6_single_board_computers.json');
 console.log('</article>');
 
 console.log('<article class="overview chapter">');
-renderLinks('./links/ch_7_web_interfaces_for_things.json');
+renderLinks('./links/ch_7_components_for_prototyping.json');
 console.log('</article>');
 
 console.log('<article class="overview chapter">');
-renderLinks('./links/ch_8_entering_the_cloud.json');
+renderLinks('./links/ch_8_nodejs_libraries_for_hardware.json');
 console.log('</article>');
 
 console.log('<article class="overview chapter">');
-renderLinks('./links/ch_9_motion_with_javascript.json');
+renderLinks('./links/ch_9_network_protocols.json');
+console.log('</article>');
+
+console.log('<article class="overview chapter">');
+renderLinks('./links/ch_10_web_interfaces_for_things.json');
+console.log('</article>');
+
+console.log('<article class="overview chapter">');
+renderLinks('./links/ch_11_entering_the_cloud.json');
+console.log('</article>');
+
+console.log('<article class="overview chapter">');
+renderLinks('./links/ch_12_motion_with_javascript.json');
 console.log('</article>');
 
 
 console.log('<article class="overview chapter">');
-renderLinks('./links/ch_10_advanced_connectivity.json');
+renderLinks('./links/ch_13_wireless_data_with_bluetooth.json');
 console.log('</article>');
 
 

96d100771764a2607b4dff65ae32d0b0eac90a6e
Patrick Mulder <mulder.patrick@gmail.com>
2016-09-01 15:33:55 +0200

    build pages from markdown

---
+++ content/ch_9.md
@@ -0,0 +1,28 @@
+
+#  Chapter 9: Exploring Network Protocols
+
+## HTTP
+
+FAQ HTTP 2.0
+Dummy data
+Overview about Node.js Web Servers
+WebSockets
+
+## The WebSockets protocol (RFC6455)
+Websocket stream
+Slides by @theturle32 on WebSockets
+RPC over web sockets
+
+## Modules
+
+http module
+request module
+ecstatic
+body
+dnode
+Shoe
+Blog post by @substack about modules for web apps
+
+## Weather stations
+
+Build your own Arduino weather station
--- render_md.js
+++ render_md.js
@@ -4,22 +4,63 @@ var fs = require('fs');
 var hyperstream = require('hyperstream');
 var fs = require('fs');
 
-// template as readable stream
-var rs = fs.createReadStream(__dirname + '/templates/index.html');
 
-var ch1Md = fs.readFileSync('content/ch_1.md');
-var hs = hyperstream({
-      '#links': marked(ch1Md.toString()) 
-});
+// markdown for ToC
+var tocMd = fs.readFileSync('content/toc.md');
 
+function renderPage(src, dest, track) {
+   // template as readable stream
+   var rs = fs.createReadStream(__dirname + '/templates/index.html');
+
+   var md = fs.readFileSync(src);
+   var hs = hyperstream({
+         '#toc': marked(tocMd.toString()),
+         '#links': marked(md.toString()) ,
+         '#track': 'mixpanel.track("' + track + '")'
+   });
+   
+   var page = fs.createWriteStream(dest);
+   rs.pipe(hs).pipe(page);
+}
+
+
+// ch1
 var ch1 = fs.createWriteStream(__dirname + '/static/worlds/index.html');
-rs.pipe(hs).pipe(ch1);
+renderPage('content/ch_1.md',
+           __dirname + '/static/worlds/index.html',
+           'worlds');
 
-var ch8Md = fs.readFileSync('content/ch_8_nodejs_libraries_for_hardware.md');
-var hs = hyperstream({
-      '#links': marked(ch8Md.toString()) 
-});
+// ch8
+renderPage('content/ch_8_nodejs_libraries_for_hardware.md',
+           __dirname + '/static/libs/index.html',
+           'libs');
 
-var ch8 = fs.createWriteStream(__dirname + '/static/libs/index.html');
-rs.pipe(hs).pipe(ch8);
-
+// ch9
+renderPage('content/ch_9.md',
+           __dirname + '/static/http/index.html',
+           'http');
+           
+// ch10
+renderPage('content/ch_10.md',
+           __dirname + '/static/webinterfaces/index.html',
+           'webintefaces');
+          
+// ch12
+renderPage('content/ch_12.md',
+           __dirname + '/static/nodebots/index.html',
+           'nodebots');
+           
+// ch13
+renderPage('content/ch_13.md',
+           __dirname + '/static/wireless/index.html',
+           'wireless');
+          
+// ch14
+renderPage('content/ch_14.md',
+           __dirname + '/static/toward/index.html',
+           'toward');
+          
+// ch15
+renderPage('content/ch_15.md',
+           __dirname + '/static/products/index.html',
+           'products');

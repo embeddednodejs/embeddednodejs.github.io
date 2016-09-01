8e4a7e97a22466f6350052ed7c0ee2fcad4ea799
Patrick Mulder <mulder.patrick@gmail.com>
2016-09-01 14:16:47 +0200

    add markdown

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,11 +1,31 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var hyperstream = require('hyperstream');
+var fs = require('fs');
+
+var markdown = require('gulp-markdown');
+
 
 gulp.task('styles', function() {
     gulp.src('sass/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./static/css/'));
 });
+
+/*
+gulp.task('content', function() {
+   var hs = hyperstream({
+         '#a': fs.createReadStream(__dirname + '/a.html'),
+         '#b': fs.createReadStream(__dirname + '/b.html')
+   });
+
+   gulp.src('content/ch_8_nodejs_libraries_for_hardware.md')
+        .pipe(markdown())
+        .pipe(hs)
+        .pipe(gulp.dest('./libs/'));
+});
+*/
+
 
 //Watch task
 gulp.task('default',function() {
